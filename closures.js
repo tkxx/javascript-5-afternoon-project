c452c8c0c578616628577d99d6da92f067b2ae83
User <user@Users-MBP.attlocal.net>
2019-07-30 22:59:08 -0500

    finished javascript-5

--- closures.js
+++ closures.js
@@ -10,34 +10,30 @@
 
 // Do not edit the code below.
 function outer() {
-  var name = 'Tyler';
+  var name = "Tyler";
   return function() {
-    return 'The original name was ' + name;
+    return "The original name was " + name;
   };
 }
 // Do not edit the code above.
-  
+
 /* 
   Above you're given a function that returns another function which has a closure over the name variable.
   Invoke outer saving the return value into another variable called 'inner'.
 */
-  
-// Code Here
 
-
+let inner = outer();
 
 //Once you do that, invoke inner.
 
-//Code Here
-
-
+inner();
 
 ////////// PROBLEM 2 //////////
 
 // Do not edit the code below.
 function callFriend(name) {
   function dial(number) {
-    return 'Calling ' + name + ' at ' + number
+    return "Calling " + name + " at " + number;
   }
   return dial;
 }
@@ -51,9 +47,7 @@ function callFriend(name) {
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
-//Code Here
-
-
+let callJake = callFriend("Jake", "435-555-9248");
 
 ////////// PROBLEM 3 //////////
 
@@ -61,18 +55,20 @@ function callFriend(name) {
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
-
-
+function makeCounter() {
+  let num = 1;
+  function increment() {
+    return num++;
+  }
+  return increment;
+}
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
-
-
+var count = makeCounter();
+count(); // 1
+count(); // 2
+count(); // 3
+count(); // 4
 
 ////////// PROBLEM 4 //////////
 
@@ -86,10 +82,15 @@ function callFriend(name) {
 */
 
 function counterFactory(value) {
-  // Code here.
-
   return {
-
+    inc: function() {
+      value++;
+      return value;
+    },
+    dec: function() {
+      value--;
+      return value;
+    }
   };
 }
 
@@ -99,8 +100,6 @@ counter = counterFactory(10);
 // counter.inc() // 13
 // counter.dec() // 12
 
-
-
 ////////// PROBLEM 5 //////////
 
 /*
@@ -109,8 +108,12 @@ counter = counterFactory(10);
   (Hint: don't forget to have a space between the firstname and lastname and a period at the end of the sentence.)
 */
 
-function motivation( firstname, lastname ) {
+function motivation(firstname, lastname) {
   var welcomeText = "You're doing awesome, keep it up";
+  function message() {
+    return welcomeText + " " + firstname + " " + lastname + ".";
+  }
+  return message;
 
   // code message function here.
 
@@ -118,9 +121,7 @@ function motivation( firstname, lastname ) {
   //return message;
 }
 
-var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
-
-
+var greeting = motivation("Billy", "Bob"); // 'You're doing awesome keep it up Billy Bob.
 
 ////////// PROBLEM 6 //////////
 
@@ -136,18 +137,27 @@ var module = (function() {
     location: "Utah"
   };
 
-  function privateMethod(){
-    return "Hi, I'm " + person.name + ", age " + person.age + " from " + person.location;
+  function privateMethod() {
+    return (
+      "Hi, I'm " +
+      person.name +
+      ", age " +
+      person.age +
+      " from " +
+      person.location
+    );
   }
 
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+    publicMethod: function() {
+      return privateMethod();
+    }
   };
 })();
 
-
+module.publicMethod();
 
 ////////// PROBLEM 7 //////////
 
@@ -162,14 +172,22 @@ function secretNumber() {
   var secret = 143;
 
   return {
-    // Code here
+    addToSecret: function(num) {
+      secret += num;
+      return secret;
+    },
+    takeAwayFromSecret: function(num) {
+      secret -= num;
+      return secret;
+    }
   };
 }
 
-
-
+//////// BLACK DIAMOND /////////
 ////////// PROBLEM 8 //////////
-  
+
+//iife
+
 /*
   Here we have a for loop that will iterate as long as i is less than or equal to 5.
   What we need to do is console.log(i) so that it logs like so:
@@ -187,7 +205,7 @@ function secretNumber() {
 */
 
 function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
+  for (let i = 0; i <= 5; i++) {
     setTimeout(function() {
       console.log(i);
     }, i * 1000);
--- constructors.js
+++ constructors.js
@@ -14,7 +14,9 @@
   The values for the make and model will come from two parameters, make and model (in that order).
 */
 
-// Code here
+function CarFactory(make, model) {
+  (this.make = make), (this.model = model);
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -33,14 +35,14 @@ function Employee(name, email, hireDate) {
   Assign the result of the invocation to a variable called bob.
 */
 
-// Code here
+let bob = new Employee("Bob", "bob@gmail.com", "01-02-98");
 
 ////////// PROBLEM 3 //////////
 
 // Do not edit the code below.
 
-var prius = new Car('Toyota', 'Prius', 2011);
-var mustang = new Car('Ford', 'Mustang', 2013);
+var prius = new Car("Toyota", "Prius", 2011);
+var mustang = new Car("Ford", "Mustang", 2013);
 prius.moveCar(); // Increments prius' move property by 10. Returns the new move property.
 mustang.moveCar(); // Increments mustang' move property by 10. Returns the new move property.
 
@@ -56,7 +58,13 @@ mustang.moveCar(); // Increments mustang' move property by 10. Returns the new m
     invoking moveCar on the right object (prius vs mustang).
 */
 
-// Code here
+function Car(make, model, year) {
+  (this.make = make), (this.model = model), (this.year = year), (this.move = 0);
+
+  this.moveCar = function() {
+    return (this.move += 10);
+  };
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -75,7 +83,11 @@ function Movie(name, genre, rating) {
   this.rating = rating;
 }
 
-// Code here
+Movie.prototype.changeRating = function(rating) {
+  let average = (this.rating + rating) / 2;
+  this.rating = average;
+  return this.rating;
+};
 
 ////////// PROBLEM 5 //////////
 
@@ -83,18 +95,33 @@ function Movie(name, genre, rating) {
 
 // Once the User constructor function is created, write a prototype method for the User function. Name this method addSavedPost. It should take in three parameters: id (a number), title (a string) and rating (a number). Use these parameters to create a new object and add it to the savedPosts array. Make sure to name the properties the same as described previously (id, title, rating).
 
-// Code here
+function User(name, age, email, savedPosts) {
+  this.name = name;
+  this.age = age;
+  this.email = email;
+  this.savedPosts = savedPosts;
+}
+
+User.prototype.addSavedPost = function(id, title, rating) {
+  this.savedPosts.push({ id, title, rating });
+};
 
 ////////// PROBLEM 6 //////////
 
 // You will be using the constructor function you just created in problem 5.
 // Write a prototype method for the User constructor function named removeSavedPost that will take in one number parameter representing the post id. Use this id to find and remove the matching object in the savedPosts array.
 
-// Code here
+User.prototype.removeSavedPost = function(id) {
+  let index = this.savedPosts.findIndex(val => val.id === id);
+  this.savedPosts.splice(index, 1);
+};
 
 ////////// PROBLEM 7 //////////
 
 // You will continue to use the constructor function you created in problem 5.
 // Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.
 
-// Code here
+User.prototype.changePostRating = function(id, rating) {
+  let index = this.savedPosts.findIndex(val => val.id === id);
+  this.savedPosts[index].rating = rating;
+};
